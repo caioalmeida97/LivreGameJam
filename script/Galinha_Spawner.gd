0e33c9ec9a1fbccf1421acffb23a5a1b0d36b8b2
taciobn <taciobarreto@hotmail.com>
2017-10-21 16:08:39 -0200

    eu

--- script/Galinha_Spawner.gd
+++ script/Galinha_Spawner.gd
@@ -9,13 +9,14 @@ func _ready():
 	pass
 	
 func _process(delta):
-	if cont < 10 :
-		var x = rand_range(0,40)
-		var z = rand_range(0,40) 
+	
+	if cont < 5 :
 		var galinha = pre_galinha.instance()
-		galinha.set_translation(Vector3(x,0,z))
-		get_owner().add_child(galinha)
 		
-		cont = cont + 1
+		
+		galinha.set_translation(Vector3(3,0,9))
+		get_owner().add_child(galinha)
+		cont += 1
+	
 		
 	pass
