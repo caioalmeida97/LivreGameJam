6c73383cad698168e83ae73a6d30ced67f812065
andredob <32962579+andredob@users.noreply.github.com>
2017-10-22 10:57:39 -0200

    subir numero de galinhas no spawn

--- scenes/Lighting.gd
+++ scenes/Lighting.gd
@@ -11,9 +11,12 @@ func _process(delta):
 	var children = []
 	children = get_node('spawn_reference').get_children()
 	print(children)
-	for i in range(1):
+	
+	for i in range(10):
 	#for i in range(children.size()):
 		get_node("Galinha_Spawner").spawn(children[i].get_translation(), int (rand_range(1, 10)))
-#		get_node("Galinha_Spawner").spawn(Vector3(0,0,0), int (rand_range(1, 10)))
+		print(get_node("Galinha_Spawner").spawn(children[i].get_translation(), int (rand_range(1, 10))))
+		
+		get_node("Galinha_Spawner").spawn(Vector3(0,0,0), int (rand_range(1, 10)))
 	set_process(false)
 	pass
