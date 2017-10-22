0af5ea6330df91196f690a875ff631837d1163e6
andredob <32962579+andredob@users.noreply.github.com>
2017-10-22 14:21:02 -0200

    spawn

--- scenes/Lighting.gd
+++ scenes/Lighting.gd
@@ -1,5 +1,5 @@
 extends Spatial
-
+var lista_galinhas = []
 
 
 func _ready():
@@ -7,16 +7,19 @@ func _ready():
 	pass
 
 func _process(delta):
-	
+	print(delta)
 	var children = []
 	children = get_node('spawn_reference').get_children()
-	print(children)
-	
+	var i = 0
+	var cont = 0
+	var listpos = 0 
 	for i in range(10):
-	#for i in range(children.size()):
-		get_node("Galinha_Spawner").spawn(children[i].get_translation(), int (rand_range(1, 10)))
-		print(get_node("Galinha_Spawner").spawn(children[i].get_translation(), int (rand_range(1, 10))))
+		var rand =int (rand_range(1,10))
+		var galinha = get_node("Galinha_Spawner").spawn(children[i].get_translation(),rand)
+		#lista_galinhas.insert(galinha, listpos)
+		listpos += 1
+		print(lista_galinhas.size())
 		
-		get_node("Galinha_Spawner").spawn(Vector3(0,0,0), int (rand_range(1, 10)))
+	#	get_node("Galinha_Spawner").spawn(Vector3(5,5,5), int (rand_range(1, 10)))
 	set_process(false)
 	pass
